3f1a5c59ca8ac4677669d6df76f631410a7392aa
Тарас Иванов <taras281@yandex.ru>
2023-08-31 18:21:45 +0200

    Настроил ликвибейз и пример создания + фикс названий папок

---
+++ impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/service/account/AccountServices.java
@@ -0,0 +1,68 @@
+package ru.skillbox.diplom.group40.social.network.impl.service.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.skillbox.diplom.group40.social.network.api.dto.account.AccountDto;
+import ru.skillbox.diplom.group40.social.network.api.dto.account.AccountDtoForGet;
+import ru.skillbox.diplom.group40.social.network.domain.account.Account;
+import ru.skillbox.diplom.group40.social.network.impl.mapper.account.MapperAccount;
+import ru.skillbox.diplom.group40.social.network.impl.repository.accaunt.AccountRepository;
+
+import javax.security.auth.login.AccountException;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServices {
+    private final MapperAccount mapperAccount;
+    private final AccountRepository accountRepository;
+
+    public AccountDto save(AccountDto accountDto) throws AccountException {
+        checkAccaunt();
+        Account account = mapperAccount.toEntity(accountDto);
+        account = accountRepository.save(account);
+        return mapperAccount.toDto(account);
+    }
+
+    public AccountDto update(AccountDto accountDto) throws AccountException {
+        checkAccaunt();
+        Account account = accountRepository.findById(accountDto.getId()).get();
+        account = mapperAccount.toEntity(accountDto);
+        account = accountRepository.save(account);
+        return mapperAccount.toDto(account);
+    }
+
+    private AccountDto getEtityAccount(Account account) {
+        account = accountRepository.save(account);
+        return mapperAccount.toDto(account);
+    }
+
+    public AccountDtoForGet get(String authorization, String email) throws AccountException {
+        checkAccaunt();
+        Account account = accountRepository.findByEmail(email);
+        AccountDtoForGet accountDtoForGet = mapperAccount.toDtoForGet(account);
+        return accountDtoForGet;
+    }
+    public AccountDto getMe(String authorization) throws AccountException {
+        Account account= accountRepository.findFirstByEmail(authorization);
+        AccountDto accountDto = mapperAccount.toDto(account);
+        return accountDto;
+    }
+
+    private void checkAccaunt() throws AccountException {
+        if(!getUnauthorized()){
+            throw new AccountException("unautorized");
+        }
+        if(!getCorrectAccountDto()){
+            throw new AccountException("badreq");
+        }
+    }
+
+    private boolean getCorrectAccountDto() {
+        return true;
+    }
+    private boolean getUnauthorized() {
+        return true;
+    }
+
+
+}
