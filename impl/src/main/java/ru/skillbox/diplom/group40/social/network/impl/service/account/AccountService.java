318ab0cf9b75efb6f4f7044a25a3c977d077ea5b
Тарас Иванов <taras281@yandex.ru>
2023-10-02 21:18:59 +0200

    its work 22

--- impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/service/account/AccountService.java
+++ impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/service/account/AccountService.java
@@ -19,12 +19,13 @@ import ru.skillbox.diplom.group40.social.network.impl.mapper.account.MapperAccou
 import ru.skillbox.diplom.group40.social.network.impl.repository.account.AccountRepository;
 import ru.skillbox.diplom.group40.social.network.impl.service.role.RoleService;
 import ru.skillbox.diplom.group40.social.network.impl.utils.auth.AuthUtil;
-import ru.skillbox.diplom.group40.social.network.impl.utils.specification.SpecificationUtils;
-
 import javax.security.auth.login.AccountException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
+
+import static ru.skillbox.diplom.group40.social.network.impl.utils.specification.SpecificationUtils.*;
+
 @Slf4j
 @Service
 @Transactional
@@ -35,43 +36,79 @@ public class AccountService {
     private final AccountRepository accountRepository;
 
     private final RoleService roleService;
-    @Transactional
+
     public AccountDto create(AccountDto accountDto) throws AccountException {
         log.info("AccountService:create() startMethod");
         Account account = mapperAccount.toEntity(accountDto);
         account.setRegistrationDate(LocalDateTime.now(ZoneId.of("Z")));
-        Set<Role> roles = roleService.getRoleSet(Arrays.asList("USER","MODERATOR"));
-        account.setRoles(roles);
+        account.setRoles(roleService.getRoleSet(Arrays.asList("USER","MODERATOR")));
         account = accountRepository.save(account);
-        log.info("AccountService:create() endMethod");
         return mapperAccount.toDto(account);
     }
 
-    @Transactional
     public AccountDto update(AccountDto accountDto) throws AccountException {
         log.info("AccountService:update() startMethod");
-        Account account = mapperAccount.toEntity(accountDto);
-        account = accountRepository.save(account);
-        return mapperAccount.toDto(account);
+        return mapperAccount.toDto(accountRepository.save(mapperAccount.toEntity(accountDto)));
     }
 
-    @Transactional
-    public AccountDto get(String email) throws AccountException {
+    public AccountDto getByEmail(String email) throws AccountException {
         log.info("AccountService:get(String email) startMethod");
         return mapperAccount.toDto(accountRepository.findFirstByEmail(email).orElseThrow(()->new AccountException("BADREUQEST")));
     }
-    @Transactional
+
     public AccountDto getId(UUID uuid) throws AccountException {
         log.info("AccountService:get(String email) startMethod");
         return mapperAccount.toDto(accountRepository.findById(uuid).orElseThrow(()->new AccountException("BADREUQEST")));
     }
-    @Transactional
+
     public AccountDto getMe() throws AccountException {
-        log.info("AccountService:getMe() startMethod");
-        UUID userId = AuthUtil.getUserId();
-        return mapperAccount.toDto(accountRepository.findById(userId).orElseThrow(()->new AccountException(BADREUQEST)));
+        log.info("AccountService: getMe() startMethod");
+        return mapperAccount.toDto(accountRepository.findById(AuthUtil.getUserId()).orElseThrow(()->new AccountException(BADREUQEST)));
     }
-    @Transactional
+    public Page<AccountDto> getResultSearch(AccountSearchDto accountSearchDto, Pageable pageable) throws AccountException  {
+        Specification spec = like(Account_.COUNTRY, accountSearchDto.getCountry())
+                .and(notEqual(Account_.ID, AuthUtil.getUserId()))
+                .and(like(Account_.FIRST_NAME, accountSearchDto.getFirstName()))
+                .and(like(Account_.LAST_NAME, accountSearchDto.getLastName()))
+                .and(like(Account_.CITY, accountSearchDto.getCity()))
+                .and(equal(Account_.EMAIL, accountSearchDto.getEmail()))
+                .and(between(Account_.BIRTH_DATE, accountSearchDto.getAgeFrom(), accountSearchDto.getAgeTo()))
+                .or(in(Account_.ID, accountSearchDto.getIds()));
+        if(accountSearchDto.getAuthor()!=null){
+            spec = spec.and(like(Account_.FIRST_NAME, accountSearchDto.getAuthor()));
+        }
+        Page<Account> accounts = accountRepository.findAll(spec, pageable);
+        return accounts.map(mapperAccount::toDto);
+    }
+    public Page<AccountDto> getAll(AccountSearchDto accountSearchDto, Pageable pageable) throws AccountException  {
+        Specification spec = equal(Account_.COUNTRY, accountSearchDto.getCountry())
+                .or(like(Account_.FIRST_NAME, accountSearchDto.getFirstName()))
+                .or(like(Account_.LAST_NAME, accountSearchDto.getLastName()))
+                .or(like(Account_.CITY, accountSearchDto.getCity()))
+                .or(equal(Account_.EMAIL, accountSearchDto.getEmail()))
+                .or(between(Account_.BIRTH_DATE, accountSearchDto.getAgeFrom(), accountSearchDto.getAgeTo()))
+                .or(in(Account_.ID, accountSearchDto.getIds()));
+        Page<Account> accounts = accountRepository.findAll(spec, pageable);
+        return accounts.map(mapperAccount::toDto);
+    }
+
+    public AccountDto putMe(AccountDto accountDto) throws AccountException{
+        log.info("AccountService:putMe() startMethod");
+        return mapperAccount.toDto(mapperAccount.rewriteEntity(accountRepository.findById(AuthUtil.getUserId()).get(), accountDto));
+    }
+
+    public boolean delete() throws AccountException{
+        log.info("AccountService:delete() startMethod");
+        accountRepository.deleteById(AuthUtil.getUserId());
+        return true;
+    }
+
+    public boolean deleteId(UUID id) throws AccountException{
+        log.info("AccountService:deleteId() startMethod");
+        accountRepository.deleteById(id);
+        return true;
+    }
+    //TODO нужен метод?
     public JwtDto getJwtDto(AuthenticateDto authenticateDto) {
         log.info("AccountService:getJwtDto() startMethod");
         Optional<Account> account = accountRepository.findFirstByEmail(authenticateDto.getEmail());
@@ -85,12 +122,12 @@ public class AccountService {
         account.get().setLastOnlineTime(LocalDateTime.now());
         return jwtDto;
     }
-
-    @Transactional
+    //TODO нужен?
     public Boolean doesAccountWithSuchEmailExist(String email){
         return accountRepository.findFirstByEmail(email).isPresent();
     }
 
+    //TODO нужен?
     private List<String> listOfRolesFromSetOfRoles(Set<Role> roles) {
         log.info("AccountService:listOfRolesFromSetOfRoles() startMethod");
         ArrayList<String> roleNames = new ArrayList<>();
@@ -99,93 +136,29 @@ public class AccountService {
         }
         return roleNames;
     }
-
-    public Page<AccountDto> getResultSearch(AccountSearchDto accountSearchDto, Pageable pageable) throws AccountException  {
-        Specification spec = SpecificationUtils
-                                               .like(Account_.COUNTRY, accountSearchDto.getCountry())
-                                               .and(SpecificationUtils.like(Account_.FIRST_NAME, accountSearchDto.getFirstName()))
-                                               .and(SpecificationUtils.like(Account_.LAST_NAME, accountSearchDto.getLastName()))
-                                               .and(SpecificationUtils.like(Account_.CITY, accountSearchDto.getCity()))
-                                               .and(SpecificationUtils.ageTo(Account_.BIRTH_DATE, accountSearchDto.getAgeTo()))
-                                               .and(SpecificationUtils.equal(Account_.EMAIL, accountSearchDto.getEmail()))
-                                               .and(SpecificationUtils.ageFrom(Account_.BIRTH_DATE, accountSearchDto.getAgeFrom()))
-                                               .or(SpecificationUtils.equalIn(Account_.ID, accountSearchDto.getIds()));
-        if(accountSearchDto.getAuthor()!=null){
-            spec = spec.and(SpecificationUtils.like(Account_.FIRST_NAME, accountSearchDto.getAuthor()));
-        }
-        Page<Account> accounts = accountRepository.findAll(spec, pageable);
-        return accounts.map(mapperAccount::toDto);
-    }
-
+    //TODO уже такой метод -> getByEmail
     public Account getAccountByEmail(String email){
         return accountRepository.findFirstByEmail(email).orElse(null);
     }
 
+    //TODO нужен?
     public void save(Account account){
         accountRepository.save(account);
     }
 
-    public AccountDto putMe(AccountDto accountDto) throws AccountException{
-        log.info("AccountService:putMe() startMethod");
-        UUID userId = AuthUtil.getUserId();
-        Account account = accountRepository.findById(userId).get();
-        account=rewrite(account, accountDto);
-        account = accountRepository.save(account);
-        return mapperAccount.toDto(account);
-    }
-    private Account rewrite(Account account, AccountDto accountDto) {
-        if(accountDto.getAbout()!=null){
-            account.setAbout(accountDto.getAbout());
-        }
-        if(accountDto.getPhone()!=null){
-            account.setPhone(accountDto.getPhone());
-        }
-        if(accountDto.getBirthDate()!=null){
-            account.setBirthDate(accountDto.getBirthDate());
-        }
-        if(accountDto.getCity()!=null){
-            account.setCity(accountDto.getCity());
-        }
-        if(accountDto.getCountry()!=null){
-            account.setCountry(accountDto.getCountry());
-        }
-        if(accountDto.getFirstName()!=null){
-            account.setFirstName(accountDto.getFirstName());
-        }
-        if(accountDto.getLastName()!=null){
-            account.setLastName(accountDto.getLastName());
-        }
-        if(account.getIsDeleted()!=accountDto.getIsDeleted()){
-            account.setIsDeleted(accountDto.getIsDeleted());
-        }
-        return account;
-    }
-
-    public boolean delete() throws AccountException{
-        UUID userId = AuthUtil.getUserId();
-        accountRepository.deleteById(userId);
-        return true;
-    }
-
-    public boolean deleteId(UUID id) throws AccountException{
-        log.info("AccountService:deleteId() startMethod");
-        accountRepository.deleteById(id);
-        return true;
-    }
-
-    public Object getStatistic(AccountStatisticRequestDto accountStatisticRequestDto) throws AccountException{
+    /*public Object getStatistic(AccountStatisticRequestDto accountStatisticRequestDto) throws AccountException{
         log.info("AccountService:deleteId() startMethod");
         Specification spec =
-                SpecificationUtils.ageFromDate(Account_.BIRTH_DATE, accountStatisticRequestDto.getDate())
-                .and(SpecificationUtils.ageFromDate(Account_.CREATED_DATE, accountStatisticRequestDto.getFirstMonth()))
-                .and(SpecificationUtils.ageToDate(Account_.CREATED_DATE, accountStatisticRequestDto.getLastMonth()));
+                 SpecificationUtils.between(Account_.BIRTH_DATE, accountStatisticRequestDto.getAgeFrom(), accountSearchDto.getAgeTo())
+                .and(SpecificationUtils.betweenDate(Account_.CREATED_BY, accountStatisticRequestDto.getFirstMonth(), accountStatisticRequestDto.getLastMonth()));
         List<Account> accounts = accountRepository.findAll(spec);
         Map<Integer, Integer> perAge = getPerAge(accounts);
         Map<Integer, Integer> perMonth = getPerMonth(accounts);
-    return null;
+        return null;
     }
 
     private Map<Integer, Integer> getPerAge(List<Account> accounts) {
+        // coments
         Map<Integer, Integer> perAge = new HashMap<>();
         for(Account account: accounts){
             int age = LocalDateTime.now().getYear()-account.getBirthDate().getYear();
@@ -211,6 +184,6 @@ public class AccountService {
             }
         }
         return perMonth;
-    }
+    }*/
 
 }
