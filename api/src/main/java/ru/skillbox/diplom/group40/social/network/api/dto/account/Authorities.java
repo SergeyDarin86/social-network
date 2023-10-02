da8c160766216674c5ee3ecb71070a5a3baa1e7a
Тарас Иванов <taras281@yandex.ru>
2023-10-02 19:39:13 +0200

    2 octoer

--- api/src/main/java/ru/skillbox/diplom/group40/social/network/api/dto/account/Authorities.java
+++ api/src/main/java/ru/skillbox/diplom/group40/social/network/api/dto/account/Authorities.java
@@ -1,6 +1,8 @@
 package ru.skillbox.diplom.group40.social.network.api.dto.account;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
@@ -9,7 +11,8 @@ import java.util.UUID;
  *
  * @taras281 Taras
  */
-@Data
+@Getter
+@Setter
 public class Authorities {
     private UUID id;
     private String authority;
