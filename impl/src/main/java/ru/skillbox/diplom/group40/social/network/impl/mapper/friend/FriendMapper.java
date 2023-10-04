5cd6ede81b0c56093446e0f070b00d87410ca8cc
TipsaUk <andrey.elistratov.mail@gmail.com>
2023-10-04 19:41:45 +0500

    Friends edit3

--- impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/mapper/friend/FriendMapper.java
+++ impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/mapper/friend/FriendMapper.java
@@ -2,10 +2,8 @@ package ru.skillbox.diplom.group40.social.network.impl.mapper.friend;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
 import ru.skillbox.diplom.group40.social.network.api.dto.friend.FriendDto;
 import ru.skillbox.diplom.group40.social.network.domain.friend.Friend;
-
 
 @Mapper(componentModel = "spring")
 public interface FriendMapper {
