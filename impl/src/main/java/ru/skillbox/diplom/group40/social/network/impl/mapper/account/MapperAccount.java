77cb80d3bb513dea6474cc3b5e06f626bbec0682
Тарас Иванов <taras281@yandex.ru>
2023-10-16 18:50:58 +0200

    kafka
    add kafka
    
    add kafka
    
    add kafka
    
    refrech code with kafka
    
    refrech code with kafka
    
    refrech code with kafka
    
    refrech code with kafka
    
    refrech code with kafka
    
    refrech code with kafka
    
    refrech code with kafka
    
    Friends edit4
    
    done
    
    refrech code with kafka
    
    fast commit
    
    fast commit
    
    fast commit
    
    fast commit
    
    fast commit
    
    fast commit
    
    fast commit
    
    new branch

--- impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/mapper/account/MapperAccount.java
+++ impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/mapper/account/MapperAccount.java
@@ -1,15 +1,12 @@
 package ru.skillbox.diplom.group40.social.network.impl.mapper.account;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import org.mapstruct.*;
 import org.springframework.stereotype.Component;
 import ru.skillbox.diplom.group40.social.network.api.dto.account.AccountDto;
+import ru.skillbox.diplom.group40.social.network.api.dto.account.AccountOnlineDto;
 import ru.skillbox.diplom.group40.social.network.api.dto.auth.RegistrationDto;
 import ru.skillbox.diplom.group40.social.network.domain.account.Account;
-import ru.skillbox.diplom.group40.social.network.domain.role.Role;
-
-import java.util.Set;
 
 
 @Log4j
@@ -26,4 +23,9 @@ public abstract class MapperAccount {
     @Mapping(target = "password", source = "dto.password1")
     public abstract AccountDto accountDtoFromRegistrationDto(RegistrationDto dto);
 
+    public abstract Account rewriteEntity(@MappingTarget Account account, AccountOnlineDto accountOnlineDto);
+
+    public abstract AccountDto AccountDtoFromAccountOnLineDto(AccountOnlineDto record);
+
+    public abstract AccountOnlineDto getAccountOnlineDtoFromAccount(Account account);
 }
---
+++ impl/src/main/java/ru/skillbox/diplom/group40/social/network/impl/service/account/AccInt.java
@@ -0,0 +1,8 @@
+package ru.skillbox.diplom.group40.social.network.impl.service.account;
+
+import ru.skillbox.diplom.group40.social.network.api.dto.account.AccountDto;
+
+@FunctionalInterface
+public interface AccInt {
+    void putmetoo(AccountDto accountDto);
+}
