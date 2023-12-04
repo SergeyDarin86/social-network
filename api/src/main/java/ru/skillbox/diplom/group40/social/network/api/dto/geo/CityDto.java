d1250542de95b510dab99d143223cecb3548bf52
Kirill <snamas@rambler.ru>
2023-12-05 00:47:07 +0700

    add geo-swagger

--- api/src/main/java/ru/skillbox/diplom/group40/social/network/api/dto/geo/CityDto.java
+++ api/src/main/java/ru/skillbox/diplom/group40/social/network/api/dto/geo/CityDto.java
@@ -1,12 +1,15 @@
 package ru.skillbox.diplom.group40.social.network.api.dto.geo;
 
-import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 import ru.skillbox.diplom.group40.social.network.api.dto.base.BaseDto;
 
 import java.util.UUID;
 
 @Data
 public class CityDto extends BaseDto{
+    @Schema(description = "Название страны")
     private String title;
+    @Schema(description = "id страны")
     private UUID countryId;
 }
